537d75b27a6b97da3d9190f22b33d462ccdbc51e
TheHawk <patricklehloenya2@gmail.com>
2025-07-05 02:05:43 +0200

    CH1: Servers

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello Web Server")
+
+	mux := http.NewServeMux()
+	//Server index.html
+	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+	mux.Handle("/app/assets/", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		msg := []byte("OK")
+		w.Write(msg)
+	})
+	server := http.Server{}
+	server.Handler = mux
+	server.Addr = ":8080"
+
+	//Listen on bind address
+	server.ListenAndServe()
 }
