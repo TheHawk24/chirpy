7f92cfd6532c6efefd3261c4af7e9b36bd2c0246
TheHawk <patricklehloenya2@gmail.com>
2025-07-07 23:59:30 +0200

    Setup Database

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 	"sync/atomic"
+
+	"github.com/TheHawk24/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	Db             *database.Queries
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -18,9 +28,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) requestsCount(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(200)
-	msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	msg := fmt.Sprintf(`<html>
+	<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>`, cfg.fileserverHits.Load())
+	//msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
 	w.Write([]byte(msg))
 }
 
@@ -32,10 +48,96 @@ func (cfg *apiConfig) resetCount(w http.ResponseWriter, r *http.Request) {
 	w.Write(msg)
 }
 
+func responseError(w http.ResponseWriter, msg_err string, resp_code int) {
+	type errorResponse struct {
+		Response string `json:"error"`
+	}
+	var e errorResponse
+	e.Response = msg_err
+	resp, err := json.Marshal(e)
+	if err != nil {
+		log.Printf("Error encoding error response: %s", err)
+		return
+	}
+	w.WriteHeader(resp_code)
+	w.Write(resp)
+}
+
+func parseRequest(w http.ResponseWriter, r *http.Request) {
+
+	type params struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var data params
+	err := decoder.Decode(&data)
+	if err != nil {
+		log.Printf("Error decoding json data: %s", err)
+		msg := "Something went wrong"
+		responseError(w, msg, 500)
+		return
+	}
+
+	if len(data.Body) > 140 {
+		msg := "Chirp is too long"
+		responseError(w, msg, 400)
+		return
+	}
+
+	not_allowed := map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+
+	words := strings.Split(data.Body, " ")
+	for i := 0; i < len(words); i++ {
+		word := strings.ToLower(words[i])
+		if not_allowed[word] {
+			words[i] = "****"
+		}
+	}
+
+	cleaned := strings.Join(words, " ")
+
+	type msgOK struct {
+		Cleaned_body string `json:"cleaned_body"`
+	}
+
+	var ok msgOK
+	ok.Cleaned_body = cleaned
+	resp, err := json.Marshal(ok)
+	if err != nil {
+		log.Printf("Faild to marshal respsonse: %s", err)
+		msg := "Somethign went wrong"
+		responseError(w, msg, 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(resp)
+}
+
 func main() {
 
+	//Database connection
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
+	dbQueries := database.New(db)
+
 	mux := http.NewServeMux()
-	api_cfg := apiConfig{}
+	api_cfg := apiConfig{
+		Db: dbQueries,
+	}
+
 	//Server index.html
 	assets_handler := http.FileServer(http.Dir("./assets"))
 	index_handler := http.FileServer(http.Dir("./"))
@@ -47,8 +149,11 @@ func main() {
 		msg := []byte("OK")
 		w.Write(msg)
 	})
-	mux.HandleFunc("GET /api/metrics", api_cfg.requestsCount)
-	mux.HandleFunc("POST /api/reset", api_cfg.resetCount)
+	mux.HandleFunc("GET /admin/metrics", api_cfg.requestsCount)
+	mux.HandleFunc("POST /admin/reset", api_cfg.resetCount)
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		parseRequest(w, r)
+	})
 	server := http.Server{}
 	server.Handler = mux
 	server.Addr = ":8080"
